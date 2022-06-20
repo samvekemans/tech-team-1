ad53ee932e6cc9158987d4ecf2204f5dc3608f12
thimogagliano <94388546+thimogagliano@users.noreply.github.com>
2022-06-20 12:25:19 +0200

    Update gulpfile.js - toevoeging plugins
    importen/toevoeging custom plugins imagemin
    toevoeging src en dest

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,33 @@
+// variabelen
+const { src, dest } = require('gulp');
+
 // importeren van gulp
 import gulp from 'gulp';
 
 // importeren van gulp-imagemin
 import imagemin from 'gulp-imagemin';
 
+// importeren standaard custom plugins van gulp-imagemin
+import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
+
 export default () => (
-    gulp.src()
-        .pipe(imagemin())
-        .pipe(gulp.dest())
+    gulp.src('./public/images/*')
+        .pipe(imagemin([
+            gifsicle({interlaced: true}),
+            mozjpeg({quality: 75, progressive: true}),
+            optipng({optimizationLevel: 5}),
+            svgo({
+                plugins: [
+                    {
+                        name: 'removeViewbox',
+                        active: true
+                    },
+                    {
+                        name: 'cleanupIDs',
+                        active: false
+                    }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('dist/images'))
 );
