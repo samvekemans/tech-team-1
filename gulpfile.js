92c09a27278ab1a8a278a533e3ce6947d3d7b849
thimogagliano <94388546+thimogagliano@users.noreply.github.com>
2022-06-20 14:27:37 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,9 @@ import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
 // importeren imageminWebp
 import imageminWebp from 'imagemin-webp';
 
+// minifying van de afbeeldingen die in /public/images staan en sturen naar /dist/images
 export default () => (
-    gulp.src('./public/images/*')
+    gulp.src('public/images/*')
         .pipe(imagemin([
             gifsicle({interlaced: true}),
             mozjpeg({quality: 75, progressive: true}),
@@ -34,11 +35,12 @@ export default () => (
                 ]
             })
         ]))
-        .pipe(gulp.dest('dist/images'))
+        .pipe(dest('public/images'))
 );
 
+// omzetten van images in /dist/images naar webp format en sturen naar dezelfde folder
 exports.default = () => (
-	gulp.src('./dist/images/*')
+	gulp.src('public/images/*')
 		.pipe(webp())
-		.pipe(dest('dist/images'))
+		.pipe(dest('public/images'))
 );
