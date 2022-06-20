9549e0f18eb3280d9b1e08344e5a1195f2f53830
your_username <your_email@gmail.com>
2022-06-20 14:37:08 +0200

    gulpfile scss and js ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-const { src, series, dest, watch, parallel } = require('gulp');
+const {
+  src,
+  series,
+  dest,
+  watch,
+  parallel
+} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const prefix = require('gulp-autoprefixer');
 const minify = require('gulp-clean-css');
 
-const eslint = require('eslint');
 const concat = require('gulp-concat');
 const sourceMaps = require('gulp-sourcemaps');
 
@@ -28,16 +33,16 @@ function style() {
     .pipe(prefix('last 2 versions'))
     .pipe(minify())
     .pipe(sourceMaps.write())
-    .pipe(concat('test.css'))
+    .pipe(concat('all.css'))
     .pipe(dest(paths.styles.dest));
 }
 
 function script() {
-  return src(paths.scripts.src, {
-    sourcemaps: true,
-  })
+  return src(paths.scripts.src)
+    .pipe(sourceMaps.init())
     .pipe(uglify())
-    .pipe(concat('main.min.js'))
+    .pipe(sourceMaps.write())
+    .pipe(concat('all.js'))
     .pipe(dest(paths.scripts.dest));
 }
 
