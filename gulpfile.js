53a94d82b982535e158b6fabc7b0f2bea4aca8f6
thimogagliano <94388546+thimogagliano@users.noreply.github.com>
2022-06-22 13:39:25 +0200

    Download packages, update gulpfile.js
    Downloaden van packages nodig voor in de gulpfile.js
    Veranderingen in gulpfile.js om het werkend te krijgen en transformeren naar webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,42 @@
-// variabelen
+const gulp = import('gulp');
+const imagemin = import('gulp-imagemin');
 const { src, dest } = require('gulp');
+const {gifsicle, mozjpeg, optipng, svgo} = import('gulp-imagemin');
 const webp = require('gulp-webp');
 
-// importeren van gulp
-import gulp from 'gulp';
+const paths = {
+    images: {
+        src: './public/images/*',
+        dest: './public/dist/images',
+        webp: './public/dist/images/webp'
+    }
+}
 
-// importeren van imagemin en standaard custom plugins van gulp-imagemin
-import imagemin, {gifsicle, mozjpeg, optipng, svgo} from 'gulp-imagemin';
+exports.default = () => {
+    return src(paths.images.src)
+    .pipe(imagemin([
+        gifsicle({interlaced: true}),
+        mozjpeg({quality: 75, progressive: true}),
+        optipng({optimizationLevel: 5}),
+        imageminWebp({quality: 75, method: 3}),
+        svgo({
+            plugins: [
+                {
+                    name: 'removeViewbox',
+                    active: true
+                },
+                {
+                    name: 'cleanupIDs',
+                    active: false
+                }
+            ]
+        })
+    ]))
+    .pipe(dest(paths.images.dest))
+};
 
-// importeren imageminWebp
-import imageminWebp from 'imagemin-webp';
-
-// minifying van de afbeeldingen die in /public/images staan en terugsturen naar dezelfde folder
-export default () => (
-    gulp.src('public/images/*')
-        .pipe(imagemin([
-            gifsicle({interlaced: true}),
-            mozjpeg({quality: 75, progressive: true}),
-            optipng({optimizationLevel: 5}),
-            imageminWebp({quality: 75, method: 3}),
-            svgo({
-                plugins: [
-                    {
-                        name: 'removeViewbox',
-                        active: true
-                    },
-                    {
-                        name: 'cleanupIDs',
-                        active: false
-                    }
-                ]
-            })
-        ]))
-        .pipe(dest('public/images'))
-);
-
-// omzetten van images in /public/images naar webp format en terugsturen naar dezelfde folder
-exports.default = () => (
-	gulp.src('public/images/*')
-		.pipe(webp())
-		.pipe(dest('public/images'))   
-);
-
-gulp
+exports.default = () => {
+    return src(paths.images.src)
+        .pipe(webp())
+        .pipe(dest(paths.images.webp)) 
+};
