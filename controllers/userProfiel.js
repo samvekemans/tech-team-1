67c0118a499965b42092416be061fa75496cb48c
samvekemans <sam.vekemans@hva.nl>
2022-06-29 12:36:04 +0200

    comments and logs removed and fixed verified styles

--- controllers/userProfiel.js
+++ controllers/userProfiel.js
@@ -3,9 +3,7 @@ const User = require('../models/user');
 const profiel = async (req, res) => {
   const careTradesTitle = 'Mijn eigenschappen:';
   const oldTradesTitle = 'Wat de ouderen verwacht van jou:';
-  console.log(req.user);
   const user = await User.findById(req.user);
-  console.log(user);
   res.render('pages/profiel', {
     user,
     careTradesTitle,
